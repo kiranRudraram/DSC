77fda6f3b1bb23894c70998ad0c1641001c80531
kiranRudraram <52860976+kiranRudraram@users.noreply.github.com>
2019-08-21 20:20:21 +0530

    Insertion sort program

---
+++ insertion_sort.cpp
@@ -0,0 +1,33 @@
+/*Insertion sort*/
+#include<iostream>
+using namespace std;
+void insertion_sort(int a[],int n)
+{
+int i,t,pos;
+for(i=0;i<n;i++)
+{
+t=a[i];
+pos=i;
+while(pos>0 && a[pos-1]>t)
+{
+a[pos]=a[pos-1];
+pos--;
+}
+a[pos]=t;
+}
+}
+int main()
+{
+int n,i;
+int list[30];
+cout<<"Enter no of elements\n";
+cin>>n;
+cout<<"Enter " << n << "numbers";
+for(i=0;i<n;i++)
+cin>>list[i];
+insertion_sort(list,n);
+cout<<"After sorting\n";
+for(i=0;i<n;i++)
+cout<<list[i]<<endl;
+return 0;
+}
